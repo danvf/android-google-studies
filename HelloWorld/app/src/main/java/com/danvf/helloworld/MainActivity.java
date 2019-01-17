16c29317baf17dea501784be63200f1c5ef3c14a
Danilo Vasconcelos de Freitas <dvf@cin.ufpe.br>
2019-01-17 12:00:18 -0300

    Update MainActivity.java

--- HelloWorld/app/src/main/java/com/danvf/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/danvf/helloworld/MainActivity.java
@@ -2,12 +2,17 @@ package com.danvf.helloworld;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "Testando 1, 2 , 3... BOOM!!!");
     }
 }
