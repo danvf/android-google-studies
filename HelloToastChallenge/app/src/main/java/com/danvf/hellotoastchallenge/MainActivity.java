bce74deef073437d5d1a8dcd4dcdf2467f97d022
Danilo Vasconcelos de Freitas <dvf@cin.ufpe.br>
2019-01-21 10:49:51 -0300

    second project initial commit

---
+++ HelloToastChallenge/app/src/main/java/com/danvf/hellotoastchallenge/MainActivity.java
@@ -0,0 +1,27 @@
+package com.danvf.hellotoastchallenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        TextView text = (TextView) findViewById(R.id.show_count);
+        int count = Integer.parseInt(String.valueOf(text)) + 1;
+        text.setText(Integer.toString(count));
+    }
+}
