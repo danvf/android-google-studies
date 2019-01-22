4c7b3cae9d5bf706f9f8f694116ef5ccce4ae2cf
Danilo Vasconcelos de Freitas <dvf@cin.ufpe.br>
2019-01-22 10:47:44 -0300

    first commit HelloConstraint

--- HelloConstraint/app/src/main/java/com/danvf/helloconstraint/MainActivity.java
+++ HelloConstraint/app/src/main/java/com/danvf/helloconstraint/MainActivity.java
@@ -1,17 +1,32 @@
 package com.danvf.helloconstraint;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
+import static android.graphics.Color.DKGRAY;
+import static android.graphics.Color.GRAY;
+
 public class MainActivity extends AppCompatActivity {
+
+    private int myCount = 0;
+    private TextView myText;
+    private Button myCountButton;
+    private Button myZeroButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myText = (TextView) findViewById(R.id.show_count);
+        myCountButton = (Button) findViewById(R.id.button_count);
+        myZeroButton = (Button) findViewById(R.id.button_zero);
     }
 
     public void showToast(View view) {
@@ -20,8 +35,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void countUp(View view) {
-        TextView text = (TextView) findViewById(R.id.show_count);
-        int count = Integer.parseInt(String.valueOf(text)) + 1;
-        text.setText(Integer.toString(count));
+
+        if (myCount == 0) {
+            myZeroButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        }
+
+        myCount++;
+
+        if ((myCount % 2) == 0) {
+            view.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        } else {
+            view.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+        }
+
+        if (myText != null)
+            myText.setText(Integer.toString(myCount));
+    }
+
+    public void setToZero(View view) {
+        myCount = 0;
+        myText.setText("0");
+        myCountButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        view.setBackgroundColor(getResources().getColor(R.color.colorGray));
     }
 }
--- HelloToastChallenge/app/src/main/java/com/danvf/hellotoastchallenge/MainActivity.java
+++ HelloToastChallenge/app/src/main/java/com/danvf/hellotoastchallenge/MainActivity.java
@@ -8,10 +8,14 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int myCount = 0;
+    private TextView myText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myText = (TextView) findViewById(R.id.show_count);
     }
 
     public void showToast(View view) {
@@ -20,8 +24,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void countUp(View view) {
-        TextView text = (TextView) findViewById(R.id.show_count);
-        int count = Integer.parseInt(String.valueOf(text)) + 1;
-        text.setText(Integer.toString(count));
+        myCount++;
+        if (myText != null)
+            myText.setText(Integer.toString(myCount));
     }
 }
