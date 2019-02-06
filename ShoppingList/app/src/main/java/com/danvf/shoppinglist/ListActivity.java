b7aa5a37ac5c7bfe058bddb76eb97288301c8f91
Danilo Vasconcelos de Freitas <dvf@cin.ufpe.br>
2019-02-06 11:40:32 -0300

    main logic implemented needing some adjustements

---
+++ ShoppingList/app/src/main/java/com/danvf/shoppinglist/ListActivity.java
@@ -0,0 +1,38 @@
+package com.danvf.shoppinglist;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+public class ListActivity extends AppCompatActivity {
+
+    public static final String EXTRA_ITEMS = "com.danvf.shoppinglist.extra.ITEMS";
+    private static boolean[] items = new boolean[4];
+    private static CheckBox[] buttons = new CheckBox[4];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        buttons[0] = findViewById(R.id.item1_toggle);
+        buttons[1] = findViewById(R.id.item2_toggle);
+        buttons[2] = findViewById(R.id.item3_toggle);
+        buttons[3] = findViewById(R.id.item4_toggle);
+    }
+
+    public void backToMain(View view) {
+        Intent goBackIntent = new Intent(this, MainActivity.class);
+
+        for (int i = 0; i < 4; i++) {
+            items[i] = buttons[i].isChecked();
+        }
+
+        goBackIntent.putExtra(EXTRA_ITEMS, items);
+        setResult(RESULT_OK, goBackIntent);
+        finish();
+    }
+}
--- ShoppingList/app/src/main/java/com/danvf/shoppinglist/MainActivity.java
+++ ShoppingList/app/src/main/java/com/danvf/shoppinglist/MainActivity.java
@@ -1,13 +1,51 @@
 package com.danvf.shoppinglist;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static boolean[] items = new boolean[4];
+    private static TextView[] texts = new TextView[4];
+    private static final int ITEM_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        texts[0] = findViewById(R.id.item1_textView);
+        texts[1] = findViewById(R.id.item2_textView);
+        texts[2] = findViewById(R.id.item3_textView);
+        texts[3] = findViewById(R.id.item4_textView);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        for (int i = 0; i < 4; i++) {
+            if (items[i]) {
+                texts[i].setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == ITEM_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                assert data != null;
+                items = data.getBooleanArrayExtra(ListActivity.EXTRA_ITEMS);
+            }
+        }
+    }
+
+    public void addToList(View view) {
+        Intent intent = new Intent(this, ListActivity.class);
+        startActivityForResult(intent, ITEM_REQUEST);
     }
 }
