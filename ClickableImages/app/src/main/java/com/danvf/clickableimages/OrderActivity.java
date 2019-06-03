3e75e86239a2165ee60faf9359baa5bc6802a6c2
Danilo Freitas <dvf@cin.ufpe.br>
2019-06-03 14:14:41 -0300

    Changing work machine

--- ClickableImages/app/src/main/java/com/danvf/clickableimages/OrderActivity.java
+++ ClickableImages/app/src/main/java/com/danvf/clickableimages/OrderActivity.java
@@ -3,9 +3,13 @@ package com.danvf.clickableimages;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class OrderActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,44 @@ public class OrderActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.order_textview);
         textView.setText(message);
+    }
+
+    public void displayToast (String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()) {
+
+            case R.id.same_day:
+                if (checked)
+                    displayToast(getString(R.string.option1_text));
+                break;
+
+            case R.id.next_day:
+                if (checked)
+                    displayToast(getString(R.string.option2_text));
+                break;
+
+            case R.id.pick_up:
+                if (checked)
+                    displayToast(getString(R.string.option3_text));
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
